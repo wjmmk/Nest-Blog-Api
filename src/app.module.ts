0626db194276361e2d2f4c02c2a1617722657448
William Mosquera <wjmmk3@gmail.com>
2023-05-03 09:07:24 -0500

    Settings Files modified

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './core/database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, ConfigModule.forRoot({ isGlobal: true }), UsersModule],
+  imports: [
+    DatabaseModule,
+    UsersModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/core/database/database.providers.ts
+++ src/core/database/database.providers.ts
@@ -1,6 +1,7 @@
 import { Sequelize } from 'sequelize-typescript';
 import { SEQUELIZE, DEVELOPMENT, TEST, PRODUCTION } from '../constants';
 import { databaseConfig } from './database.config';
+import { User } from 'src/modules/users/user.entity';
 
 export const databaseProviders: any = [
   {
@@ -22,7 +23,7 @@ export const databaseProviders: any = [
       }
 
       const sequelize = new Sequelize(config);
-      sequelize.addModels(['models goes here']);
+      sequelize.addModels([User]);
       await sequelize.sync();
       return sequelize;
     },
